e127fd0cd013a88d4be9b2d05e98ef8b82a6c574
Mohamed Taher Othmen <mohamedtaher.othman@gmail.com>
2025-04-11 17:36:16 +0100

    class-based PDO Connection

--- backend/connection.php
+++ backend/connection.php
@@ -1,10 +1,16 @@
 <?php
-    require './config.php';
-    try {
-        $connect = new PDO("mysql:host=$host;dbname=$db;charset=UTF8", $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-        if ($connect)
-            echo "Connected to the $db database successfully!<br>";
-    }catch (PDOException $e) {
-        echo $e->getMessage();
+    require_once './config.php';
+    class Connection {
+        public static function connectDataBase ($host, $user, $password, $db){
+            $dsn = "mysql:host=$host;dbname=$db;charset=UTF8";
+            $option = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+            try{
+                return new PDO($dsn, $user, $password, $option);
+            }catch (PDOException $e){
+                echo"ERROR MSG : " . $e->getMessage();
+                die();
+            }
+        }
     }
+    return Connection::connectDataBase($host, $db, $user, $password, $db);
 ?>
